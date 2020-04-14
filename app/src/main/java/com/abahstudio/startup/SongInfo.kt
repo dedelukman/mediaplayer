913d2bea0cbb29d00ee29b36453d59c109f77aa6
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-04-14 09:19:45 +0700

    initial innit

---
+++ app/src/main/java/com/abahstudio/startup/SongInfo.kt
@@ -0,0 +1,8 @@
+package com.abahstudio.startup
+
+class SongInfo(Title: String, AuthorName: String, SongURL: String) {
+    var Title:String?= Title
+    var AuthorName:String?= AuthorName
+    var SongURL:String?= SongURL
+
+}
